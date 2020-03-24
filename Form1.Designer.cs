bcc27ff46fc0c0ef813ea8671802f6b29962da07
Shuibin Long <dragon60066@163.com>
2020-03-24 19:40:11 +0800

    Add loading image format

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,9 +42,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.pictureBox1.Location = new System.Drawing.Point(18, 80);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(439, 439);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
@@ -50,7 +54,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(18, 32);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(440, 21);
@@ -62,9 +66,9 @@
             this.label1.Location = new System.Drawing.Point(16, 18);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 2;
-            this.label1.Text = "图片路径:";
+            this.label1.Text = "图片路径：";
             // 
             // label2
             // 
@@ -72,22 +76,55 @@
             this.label2.Location = new System.Drawing.Point(16, 66);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.Size = new System.Drawing.Size(113, 12);
             this.label2.TabIndex = 3;
-            this.label2.Text = "将图片拖放到此处:";
+            this.label2.Text = "将图片拖放到此处：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 535);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "导出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 540);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "默认分辨率：未知";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(209, 540);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "格式：未知";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(476, 548);
+            this.ClientSize = new System.Drawing.Size(476, 570);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Image Modifier v1.0";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -101,6 +138,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace ImgModifier
 {
     public partial class Form1 : Form
     {
+        public static bool flag = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,8 +27,11 @@ namespace ImgModifier
         private void pictureBox1_DragDrop(object sender, DragEventArgs e)
         {
             string fileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
-            this.pictureBox1.Image = Image.FromFile(fileName);
+            pictureBox1.Image = Image.FromFile(fileName);
             textBox1.Text = fileName;
+            label3.Text = "默认分辨率：" + pictureBox1.Image.Width.ToString() + "x" + pictureBox1.Image.Height.ToString();
+            flag = pictureBox1.Image.Width >= pictureBox1.Image.Height;
+            label4.Text = "格式：" +  (flag ? "横板" : "竖版");
         }
 
         private void pictureBox1_DragEnter(object sender, DragEventArgs e)
@@ -39,6 +44,16 @@ namespace ImgModifier
             {
                 e.Effect = DragDropEffects.None;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String path = "C:/Users/drago/Desktop/test.jpg";
+            Image newImg = new Bitmap(768, 1366);
+            Graphics graphics = Graphics.FromImage(newImg);
+            graphics.DrawImage(pictureBox1.Image, 0, 0, 768, 1366);
+            newImg.Save(path);
+            newImg.Dispose();
         }
     }
 }
