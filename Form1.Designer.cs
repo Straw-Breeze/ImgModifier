dcb106a966225e035f770a7afcd3be98f60b8642
Shuibin Long <dragon60066@163.com>
2020-04-20 17:04:09 +0800

    Add image capture module

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,16 +37,34 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.pictureBox1.Location = new System.Drawing.Point(18, 80);
+            this.pictureBox1.Location = new System.Drawing.Point(7, 70);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(439, 439);
+            this.pictureBox1.Size = new System.Drawing.Size(440, 440);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -55,7 +73,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(18, 32);
+            this.textBox1.Location = new System.Drawing.Point(7, 22);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
@@ -65,7 +83,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Location = new System.Drawing.Point(5, 8);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 12);
@@ -75,7 +93,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 66);
+            this.label2.Location = new System.Drawing.Point(5, 56);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(113, 12);
@@ -84,7 +102,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(383, 535);
+            this.button1.Location = new System.Drawing.Point(372, 522);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -95,7 +113,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 540);
+            this.label3.Location = new System.Drawing.Point(5, 527);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(101, 12);
@@ -105,7 +123,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(184, 540);
+            this.label4.Location = new System.Drawing.Point(173, 527);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(65, 12);
@@ -114,7 +132,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(302, 535);
+            this.button2.Location = new System.Drawing.Point(291, 522);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
@@ -122,29 +140,176 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(462, 578);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(454, 552);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "分辨率转换";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.trackBar1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(454, 552);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "图片裁剪";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(440, 440);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.SizeChanged += new System.EventHandler(this.pictureBox2_SizeChanged);
+            this.pictureBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox2_DragDrop);
+            this.pictureBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox2_DragEnter);
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 22);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(440, 21);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(141, 527);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "格式：未知";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 8);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "图片路径：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 527);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 12);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "默认分辨率：未知";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(5, 56);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 12);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "将图片拖放到此处：";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(7, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(440, 440);
+            this.panel1.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(372, 522);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.BackColor = System.Drawing.Color.White;
+            this.trackBar1.Location = new System.Drawing.Point(221, 522);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(145, 23);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(476, 570);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(458, 576);
+            this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Image Modifier v1.1";
+            this.Text = "Image Modifier v2.0";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -158,6 +323,18 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,12 @@ namespace ImgModifier
         public static bool flag = false;
         public static int[,] formatHorizon = new int[3,2]{ { 1366, 768 }, { 448, 252 }, { 1080, 680 } };
         public static int[,] formatVertical = new int[2, 2] { { 960, 1440 }, { 448, 672 } };
+        public Rectangle rectangle;
+        public const int areaWidth = 440;
+        public const int areaHeight = 440;
+        public static bool canMove = false;
+        public static int mouseX = 0;
+        public static int mouseY = 0;
 
         public Form1()
         {
@@ -24,6 +30,7 @@ namespace ImgModifier
         private void Form1_Load(object sender, EventArgs e)
         {
             pictureBox1.AllowDrop = true;
+            pictureBox2.AllowDrop = true;
         }
 
         private void pictureBox1_DragDrop(object sender, DragEventArgs e)
@@ -50,6 +57,11 @@ namespace ImgModifier
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("输入图片不能为空！");
+                return;
+            }
             string[] tmpPath = getImgCatalog(textBox1.Text);
             for (int i = 0; i < 2; i++)
             {
@@ -70,6 +82,11 @@ namespace ImgModifier
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("输入图片不能为空！");
+                return;
+            }
             string[] tmpPath = getImgCatalog(textBox1.Text);
             int x = formatHorizon[2, 0];
             int y = formatHorizon[2, 1];
@@ -91,6 +108,149 @@ namespace ImgModifier
                 }
             }
             return new string[] { "error", "error" };
+        }
+
+        private void pictureBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            string fileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            pictureBox2.Image = Image.FromFile(fileName);
+            textBox2.Text = fileName;
+            label7.Text = "默认分辨率：" + pictureBox2.Image.Width.ToString() + "x" + pictureBox2.Image.Height.ToString();
+            flag = pictureBox2.Image.Width >= pictureBox2.Image.Height;
+            label5.Text = "格式：" + (flag ? "横板" : "竖版");
+            Bitmap bitmap = new Bitmap(Image.FromFile(fileName));
+            if (flag && bitmap.Width > areaWidth)
+            {
+                pictureBox2.Width = areaWidth;
+                pictureBox2.Height = bitmap.Height * areaWidth / bitmap.Width;
+            }
+            if (!flag && bitmap.Height > areaHeight)
+            {
+                pictureBox2.Height = areaHeight;
+                pictureBox2.Width = bitmap.Width * areaHeight / bitmap.Height;
+            }
+            if (bitmap.Width < pictureBox2.Width)
+            {
+                pictureBox2.Width = bitmap.Width;
+            }
+            if (bitmap.Height < pictureBox2.Height)
+            {
+                pictureBox2.Height = bitmap.Height;
+            }
+            rectangle = new Rectangle(0, 0, 0, 0);
+            rectangle.Width = (int)(Math.Min(pictureBox2.Width, pictureBox2.Height) * 0.8);
+            rectangle.Height = rectangle.Width;
+            trackBar1.Value = (int)(trackBar1.Maximum * 0.8);
+        }
+        private void pictureBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            if (rectangle != null)
+            {
+                e.Graphics.DrawRectangle(new Pen(new SolidBrush(Color.Black), 1), this.rectangle);
+            }
+        }
+
+        private void pictureBox2_SizeChanged(object sender, EventArgs e)
+        {
+            pictureBox2.Left = panel1.Width / 2 - pictureBox2.Width / 2;
+            pictureBox2.Top = panel1.Height / 2 - pictureBox2.Height / 2;
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point point = pictureBox2.PointToClient(Control.MousePosition);
+            if (point.X < rectangle.X || point.X > rectangle.X + rectangle.Width || point.Y < rectangle.Y || point.Y > rectangle.Y + rectangle.Height)
+            {
+                canMove = false;
+            }
+            else
+            {
+                canMove = true;
+                mouseX = point.X;
+                mouseY = point.Y;
+            }
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (canMove)
+            {
+                Point point = pictureBox2.PointToClient(Control.MousePosition);
+                rectangle.X += point.X - mouseX;
+                rectangle.Y += point.Y - mouseY;
+                mouseX = point.X;
+                mouseY = point.Y;
+                rectangle.X = rectangle.X < 0 ? 0 : rectangle.X;
+                rectangle.Y = rectangle.Y < 0 ? 0 : rectangle.Y;
+                rectangle.X = rectangle.X + rectangle.Width >= pictureBox2.Width ? pictureBox2.Width - rectangle.Width - 1 : rectangle.X;
+                rectangle.Y = rectangle.Y + rectangle.Height >= pictureBox2.Height ? pictureBox2.Height - rectangle.Height - 1 : rectangle.Y;
+                pictureBox2.Invalidate();
+            }
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            canMove = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image == null)
+            {
+                MessageBox.Show("输入图片不能为空！");
+                return;
+            }
+            int sx = pictureBox2.Image.Width * rectangle.X / pictureBox2.Width;
+            int sy = pictureBox2.Image.Height * rectangle.Y / pictureBox2.Height;
+            int w = pictureBox2.Image.Width * rectangle.Width / pictureBox2.Width;
+            int h = pictureBox2.Image.Height * rectangle.Height / pictureBox2.Height;
+            int W = 540, H = 540;
+            string[] tmpPath = getImgCatalog(textBox2.Text);
+            Image newImg = new Bitmap(W, H);
+            Graphics graphics = Graphics.FromImage(newImg);
+            graphics.DrawImage(pictureBox2.Image, new Rectangle(0, 0, W, H), new Rectangle(sx, sy, w, h), GraphicsUnit.Pixel);
+            newImg.Save(tmpPath[0] + "_" + W.ToString() + "x" + H.ToString() + tmpPath[1]);
+            newImg.Dispose();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (rectangle == null || pictureBox2.Image == null)
+            {
+                return;
+            }
+            double rate = trackBar1.Value * 1.0 / trackBar1.Maximum;
+            if (flag)
+            {
+                rectangle.Height = Math.Min((int)(pictureBox2.Height * rate), pictureBox2.Height - 1);
+                rectangle.Width = rectangle.Height;
+            }
+            else
+            {
+                rectangle.Width = Math.Min((int)(pictureBox2.Width * rate), pictureBox2.Width - 1);
+                rectangle.Height = rectangle.Width;
+            }
+            if (rectangle.X + rectangle.Width >= pictureBox2.Width)
+            {
+                rectangle.X = pictureBox2.Width - rectangle.Width - 1;
+            }
+            if (rectangle.Y + rectangle.Height >= pictureBox2.Height)
+            {
+                rectangle.Y = pictureBox2.Height - rectangle.Height - 1;
+            }
+            pictureBox2.Invalidate();
         }
     }
 }
